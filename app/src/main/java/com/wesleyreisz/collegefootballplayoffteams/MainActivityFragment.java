647de6e9c908c4eab11af1fafc6ec716f8dfdc29
Wesley Reisz <wes@wesleyreisz.com>
2016-11-28 07:30:23 -0500

    Working List

--- app/src/main/java/com/wesleyreisz/collegefootballplayoffteams/MainActivityFragment.java
+++ app/src/main/java/com/wesleyreisz/collegefootballplayoffteams/MainActivityFragment.java
@@ -27,11 +27,9 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mTeamsReference = mRootRef.child("teams");
 
-    ListView teamsListView;
-    ListAdapter teamsListAdapter;
+    FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mRef = mFirebaseDatabase.getReference().child("teams");
 
     public MainActivityFragment() {
     }
@@ -40,21 +38,15 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        teamsListView = (ListView)view.findViewById(R.id.listViewTeams);
-        teamsListAdapter = new FirebaseListAdapter<Team>(getActivity(),Team.class,R.layout.team_layout,mTeamsReference) {
+        ListView listView = (ListView) view.findViewById(R.id.listViewTeams);
+        ListAdapter listAdapter = new FirebaseListAdapter<Team>(getActivity(),Team.class,R.layout.team_layout,mRef) {
             @Override
             protected void populateView(View v, Team model, int position) {
-                Log.d(Constants.TAG,model.toString());
-                TextView teamName = (TextView)v.findViewById(R.id.teamName);
-                teamName.setText(model.getName());
-                TextView teamMascot = (TextView)v.findViewById(R.id.teamNickName);
-                teamMascot.setText(model.getMascot());
-                TextView teamRecord = (TextView)v.findViewById(R.id.teamRecord);
-                teamRecord.setText("Record: " + model.getWins() + "-" + model.getLoses());
+                TextView textViewTeamName = (TextView)v.findViewById(R.id.teamName);
+                textViewTeamName.setText(model.getName());
             }
         };
-        teamsListView.setAdapter(teamsListAdapter);
-
+        listView.setAdapter(listAdapter);
         return view;
     }
 
